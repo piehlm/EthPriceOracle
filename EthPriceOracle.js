8a1fb05509eb299ef2cd1192ca52596a4c865da2
Mike Piehl <piehlm@paperstreetenterprises.com>
2021-06-07 14:22:46 -0400

    lesson 15-chapter 10
    added init function
    added retrieveLatestEthPrice

--- EthPriceOracle.js
+++ EthPriceOracle.js
@@ -13,6 +13,17 @@ async function getOracleContract (web3js) {
   return new web3js.eth.Contract(OracleJSON.abi, OracleJSON.networks[networkId].address)
 }
 
+async function retrieveLatestEthPrice () {
+  const resp = await axios({
+    url: 'https://api.binance.com/api/v3/ticker/price',
+    params: {
+      symbol: 'ETHUSDT'
+    },
+    method: 'get'
+  })
+  return resp.data.price
+}
+
 async function filterEvents (oracleContract, web3js) {
   oracleContract.events.GetLatestEthPriceEvent(async (err, event) => {
     if (err) {
@@ -52,10 +63,30 @@ async function processRequest (oracleContract, ownerAddress, id, callerAddress) 
       return
     } catch (error) {
       if (retries === MAX_RETRIES - 1) {
-          await setLatestEthPrice(oracleContract, callerAddress, ownerAddress, '0', id)
-          return
+        await setLatestEthPrice(oracleContract, callerAddress, ownerAddress, '0', id)
+        return
       }
       retries++
     }
   }
+}
+
+async function setLatestEthPrice (oracleContract, callerAddress, ownerAddress, ethPrice, id) {
+  ethPrice = ethPrice.replace('.', '')
+  const multiplier = new BN(10**10, 10)
+  const ethPriceInt = (new BN(parseInt(ethPrice), 10)).mul(multiplier)
+  const idInt = new BN(parseInt(id))
+  try {
+    await oracleContract.methods.setLatestEthPrice(ethPriceInt.toString(), callerAddress, idInt.toString()).send({ from: ownerAddress })
+  } catch (error) {
+    console.log('Error encountered while calling setLatestEthPrice.')
+    // Do some error handling
+  }
+}
+
+async function init () {
+  const { ownerAddress, web3js, client } = common.loadAccount(PRIVATE_KEY_FILE_NAME)
+  const oracleContract = await getOracleContract(web3js)
+  filterEvents(oracleContract, web3js)
+  return { oracleContract, ownerAddress, client }
 }
